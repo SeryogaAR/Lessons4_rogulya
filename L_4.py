42381b741cbf30f0ca2a7985f7006b4ba944040f
Serhiy Rogulya <serhiyrogulya@gmail.com>
2021-02-06 23:30:04 +0200

    Hello

---
+++ L_4.py
@@ -0,0 +1,5 @@
+my_list = [11,21,31,114,55321,34]
+
+for value in my_list:
+    if value > 100:
+        print(type(value), value)
--- lesson4.py
+++ lesson4.py
@@ -1,1 +1,6 @@
-print("HEllo!")
+my_list = [11,21,31,114,55321,34]
+my_results = []
+
+for result in my_list:
+    if result > 100:
+        print(my_results.append(result))
