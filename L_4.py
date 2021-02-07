afb210000847166c9ed85aaf9be14156f44454f1
Serhiy Rogulya <serhiyrogulya@gmail.com>
2021-02-07 10:48:40 +0200

    Hello

--- L_4.py
+++ L_4.py
@@ -2,4 +2,18 @@ my_list = [11,21,31,114,55321,34]
 
 for value in my_list:
     if value > 100:
-        print(type(value), value)
+        print(value)
+
+############
+
+my_list = [11,21,31,114,55321,34]
+my_results = []
+
+for value in my_list:
+    if value > 100:
+        my_results.append(value)
+print(my_results)
+
+##############
+
+
--- lesson4.py
+++ lesson4.py
@@ -1,6 +1,7 @@
 my_list = [11,21,31,114,55321,34]
-my_results = []
 
-for result in my_list:
-    if result > 100:
-        print(my_results.append(result))
+index = range(len(my_list))2:
+    my_list.append("0")
+else:
+    my_list(index[-1 * -2])
+print(index)
